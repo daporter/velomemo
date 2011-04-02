0caa06625ea0a1d634d85d74b31b2e8d6af683f2
David Porter <david.a.porter@gmail.com>
2011-04-02 11:27:46 +1100

    Add Parser specs

---
+++ spec/parser_spec.rb
@@ -0,0 +1,15 @@
+require 'velomemo'
+
+describe Velomemo::Parser do
+  describe '#parse' do
+    it 'returns a Ride for each YAML item' do
+      yaml = <<-END
+        - date: 2011-03-10
+          distance: 22
+        - date: 2011-03-11
+          distance: 31
+      END
+      Velomemo::Parser.parse(yaml).should have(2).items
+    end
+  end
+end
