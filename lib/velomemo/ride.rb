9185ff0875d592e14bb3d24ec88bdfd124574408
David Porter <david.a.porter@gmail.com>
2011-04-02 14:37:08 +1100

    Ride align each label at column 20

--- lib/velomemo/ride.rb
+++ lib/velomemo/ride.rb
@@ -1,15 +1,15 @@
 module Velomemo
 
   class Ride
+    LABEL_WIDTH = 20
 
     def initialize(data)
       @data = data
     end
 
     def to_s
-      @data.map { |k,v| "#{k}: #{v}" }.join("\n")
+      @data.map { |k,v| sprintf("%#{LABEL_WIDTH}s: %s", k, v) }.join("\n")
     end
-
   end
 
 end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -2,10 +2,18 @@ require "velomemo"
 
 describe Velomemo::Ride do
   describe "#to_s" do
-    it "displays each key-value pair separated by a newline" do
-      ride = Velomemo::Ride.new(:notes => "foo", :distance => 30)
-      ride.to_s.should =~ /^\s*notes:\s+foo$/
-      ride.to_s.should =~ /^\s*distance:\s+30$/
+    before(:each) do
+      @ride = Velomemo::Ride.new(:notes => "foo", :distance => 30)
+    end
+
+    it "displays each label-value pair separated by a newline" do
+      @ride.to_s.should =~ /^\s*notes:\s+foo$/
+      @ride.to_s.should =~ /^\s*distance:\s+30$/
+    end
+
+    it "right-aligns each label at column 20" do
+      @ride.to_s.should =~ /^               notes: foo$/
+      @ride.to_s.should =~ /^            distance: 30$/
     end
   end
 end
