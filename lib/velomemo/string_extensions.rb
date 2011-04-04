da2899b2913466d8925477b4f7729253bd0c7ab3
David Porter <david.a.porter@gmail.com>
2011-04-04 14:16:31 +1000

    Use a module in String monkey patch
    This makes it easier to see where monkey patched methods are defined.

--- lib/velomemo/string_extensions.rb
+++ lib/velomemo/string_extensions.rb
@@ -1,11 +1,15 @@
-class String
+module Velomemo
+  module String
+    def word_wrap(width=78)
+      gsub(/\s+/, " ").gsub(/(.{1,#{width}})( |\Z)/, "\\1\n")
+    end
+  end
+end
 
+class String
   if instance_methods.include?(:word_wrap)
     raise "Method 'word_wrap' exists. String Extensions will not override!"
   end
 
-  def word_wrap(width=78)
-    gsub(/\s+/, " ").gsub(/(.{1,#{width}})( |\Z)/, "\\1\n")
-  end
-
+  include Velomemo::String
 end
