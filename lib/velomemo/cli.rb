4033a7f645c8602fa6aa11e34adb1cf487476773
David Porter <david.a.porter@gmail.com>
2011-04-17 22:54:26 +1000

    Use Trollop instead of OptionParser
    Trollop is simpler and more powerful.

--- lib/velomemo/cli.rb
+++ lib/velomemo/cli.rb
@@ -1,58 +1,31 @@
-require 'optparse'
-require 'optparse/date'
-require 'ostruct'
+require "trollop"
 
 module Velomemo
 
   class CLI
     def self.execute(stdout, stderr, args=[])
-      options = parse_options(stdout, stderr, args)
-      data    = File.read(options.file)
+      options = parse_options(args)
+      data    = File.read(options[:file])
       period  = Period.new(options.begin, options.end)
       rides   = Velomemo::Parser.parse(data)
 
       stdout.puts Velomemo::Report.new(rides, period)
     end
 
-    def self.parse_options(stdout, stderr, args)
-      options = OpenStruct.new
+    def self.parse_options(args)
+      options = Trollop::options(args) do
+        version "#{File.basename($0)} #{Velomemo::VERSION}"
 
-      opts = OptionParser.new
-      opts.banner = "Usage: #{File.basename($0)} [options]"
+        banner <<-EOS.undent
+          Usage: #{File.basename($0)} [options]
+        EOS
 
-      opts.on("-b", "--begin DATE",
-              Date,
-              "Filter entries on or after DATE") do |b|
-        options.begin = b
+        opt :file,  "File containing ride data",     :type => ::String, :required => true
+        opt :begin, "Ignore rides before this date", :type => Date
+        opt :end,   "Ignore rides after this date",  :type => Date
       end
 
-      opts.on("-e", "--end DATE",
-              Date,
-              "Filter entries before or on DATE") do |e|
-        options.end = e
-      end
-
-      opts.on("-f", "--file FILE", "Read entries from FILE") do |f|
-        options.file = f
-      end
-
-      opts.on_tail("-h", "--help", "Show this message") do
-        stdout.puts opts
-        exit
-      end
-
-      opts.on_tail("--version", "Show version") do
-        stdout.puts "#{File.basename($0)} #{Velomemo::VERSION}"
-        exit
-      end
-
-      begin
-        opts.parse!(args)
-      rescue OptionParser::InvalidOption,
-        OptionParser::MissingArgument,
-        OptionParser::InvalidArgument => e
-        abort "Error: #{e.message}"
-      end
+      Trollop::die :file, "must exist" unless File.exist?(options[:file])
 
       options
     end
--- lib/velomemo/string_extensions.rb
+++ lib/velomemo/string_extensions.rb
@@ -3,12 +3,18 @@ module Velomemo
     def word_wrap(width=78)
       gsub(/\s+/, " ").gsub(/(.{1,#{width}})( |\Z)/, "\\1\n")
     end
+
+    def undent
+      gsub(/^.{#{slice(/^ +/).length}}/, '')
+    end
   end
 end
 
 class String
-  if instance_methods.include?(:word_wrap)
-    raise "Method 'word_wrap' exists. String Extensions will not override!"
+  [:word_wrap, :undent].each do |method|
+    if instance_methods.include?(method)
+      raise "Method '#{method}' exists. String Extensions will not override!"
+    end
   end
 
   include Velomemo::String
